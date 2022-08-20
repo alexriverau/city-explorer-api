04fac2b7ad0c7ceca934bd350886d39f031058a0
Alejandro Rivera <alexrivera78@gmail.com>
2022-08-20 16:38:19 -0500

    adds cache

--- modules/weather.js
+++ modules/weather.js
@@ -1,25 +1,36 @@
 'use strict';
 
 const axios = require('axios');
-let cache = require('./cache');
+let cache = require('./cache.js');
 
-async function getWeather(req, res, next) {
-  const city = req.query.searchQuery;
-  const url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&city=${city}&days=5`;
+function getWeather(req) {
+  const { lat, lon } = req.query;
+  const key = 'weather-' + lat + lon;
+  const url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lang=en&lat=${lat}${lon}&days=5`;
 
-  axios
-    .get(url)
-    .then((weatherRes) => {
-      const weatherArr = weatherRes.data.data.map(
-        (weather) => new Forecast(weather)
-      );
-      res.status(200).send(weatherArr);
-    })
-    .catch((error) => {
-      next(error);
-    });
+  if (cache[key] && Date.now() - cache[key].timestamp < 50000) {
+    console.log('Cache hit');
+  } else {
+    console.log('Cache miss');
+    cache[key] = {};
+    cache[key].timestamp = Date.now();
+    cache[key].data = axios
+      .get(url)
+      .then((weatherRes) => parseWeather(weatherRes.data));
+  }
+  return cache[key].data;
 }
 
+function parseWeather(weatherRes) {
+  try {
+    const weatherArray = weatherRes.data.map((weather) => {
+      return new Forecast(weather);
+    });
+    return Promise.resolve(weatherArray);
+  } catch (error) {
+    return Promise.reject(error);
+  }
+}
 class Forecast {
   constructor(obj) {
     this.description =
