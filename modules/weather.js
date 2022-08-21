7312b6ea6a91e449b5342615abc7ea086bdc4fda
Alejandro Rivera <alexrivera78@gmail.com>
2022-08-21 11:17:06 -0500

    fixes rendering weather

--- modules/weather.js
+++ modules/weather.js
@@ -1,46 +1,34 @@
 'use strict';
 
 const axios = require('axios');
-let cache = require('./cache.js');
 
-function getWeather(req) {
-  const { lat, lon } = req.query;
-  const key = 'weather-' + lat + lon;
-  const url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lang=en&lat=${lat}${lon}&days=5`;
+async function getWeather(req, res, next) {
+  const city = req.query.searchQuery;
+  const url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&city=${city}&days=5`;
 
-  if (cache[key] && Date.now() - cache[key].timestamp < 50000) {
-    console.log('Cache hit');
-  } else {
-    console.log('Cache miss');
-    cache[key] = {};
-    cache[key].timestamp = Date.now();
-    cache[key].data = axios
-      .get(url)
-      .then((weatherRes) => parseWeather(weatherRes.data));
-  }
-  return cache[key].data;
-}
-
-function parseWeather(weatherRes) {
-  try {
-    const weatherArray = weatherRes.data.map((weather) => {
-      return new Forecast(weather);
+  axios
+    .get(url)
+    .then((weatherRes) => {
+      const weatherArr = weatherRes.data.data.map(
+        (weather) => new Forecast(weather)
+      );
+      res.status(200).send(weatherArr);
+    })
+    .catch((error) => {
+      next(error);
     });
-    return Promise.resolve(weatherArray);
-  } catch (error) {
-    return Promise.reject(error);
-  }
-}
-class Forecast {
-  constructor(obj) {
-    this.description =
-      'Low of ' +
-      obj.low_temp +
-      ', high of ' +
-      obj.high_temp +
-      ' with ' +
-      obj.weather.description.toLowerCase();
-    this.date = obj.datetime;
+
+  class Forecast {
+    constructor(obj) {
+      this.description =
+        'Low of ' +
+        obj.low_temp +
+        ', high of ' +
+        obj.high_temp +
+        ' with ' +
+        obj.weather.description.toLowerCase();
+      this.date = obj.datetime;
+    }
   }
 }
 
